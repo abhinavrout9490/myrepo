3aea2e4d86c56ffb8a57cf63ef68660d772df616
Abhinav Rout <abhinavrout9490@gmail.com>
2024-08-17 18:02:31 +0530

    added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 data = {
-            'Course Name': ['DevOps', 'Azure'], 
+            'Course Name': ['DevOps', 'Azure2'], 
             'Duration': ['30 days', '30days']
     }
 df = pd.DataFrame(data)
